b0247173a671a847ba7789922c479381aa0ea688
tayuta <tayutayuta09@gmail.com>
2020-04-15 12:40:08 +0900

    upd genarete property を削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,10 +3,6 @@ import pkg from './package'
 export default {
   mode: 'spa',
 
-  router: {
-    base: '/portfolio/'
-  },
-
   /*
    ** Headers of the page
    */
@@ -18,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/portfolio/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
